2118ba734d0be66facf11e7e59070c47da5d1620
Zander Peterson <zander.j.peterson@gmail.com>
2024-07-31 18:27:07 +1200

    Added Functions to Game Class
    Made the Game Class work with the Main Menu state whilst also adding in some missing Type Hints.

--- src/game.py
+++ src/game.py
@@ -2,9 +2,29 @@
 
 import pygame
 
+from states.main_menu import MainMenuState
+
 class Game():
     """A Class intended to handle game related functions."""
 
-    def __init__(self, window):
+    def __init__(self, window) -> None:
         self.window = window
-        self.states = {}
+        self.states = {
+            "main_menu": MainMenuState(self)
+        }
+        self.current_state = self.states["main_menu"]
+        self.current_state.enter()
+
+    def change_state(self, to_state: str) -> None:
+        self.current_state.exit()
+        self.current_state = self.states[to_state]
+        self.current_state.enter()
+
+    def handle_event(self, event) -> None:
+        self.current_state.handle_event(event)
+
+    def update(self) -> None:
+        self.current_state.update()
+
+    def render(self) -> None:
+        self.current_state.render(self.screen)
--- src/states/game_state.py
+++ src/states/game_state.py
@@ -1,25 +1,25 @@
 class GameState():
     """A class intended to handle different Game States."""
 
-    def __init__(self, game):
+    def __init__(self, game) -> None:
         self.game = game
 
-    def enter(self):
+    def enter(self) -> None:
         """Called when entering the state."""
         pass
 
-    def exit(self):
+    def exit(self) -> None:
         """Called when exiting the state."""
         pass
 
-    def update(self):
+    def update(self) -> None:
         """Called whenever updating the state."""
         pass
 
-    def render(self):
+    def render(self) -> None:
         """Called whenever rendering the state."""
         pass
 
-    def handle_event(self):
+    def handle_event(self) -> None:
         """Called whenever an event needs to be handled."""
         pass
