f1ce58a2b5b5aa8558e2b7abe19f23c1d03afa76
Zander Peterson <zander.j.peterson@gmail.com>
2024-08-07 11:51:53 +1200

    Added Documentation
    Added some comments and documentation. Made the decision to utilise Radians henceforth as opposed to degrees.

--- src/objects/game_object.py
+++ src/objects/game_object.py
@@ -13,23 +13,27 @@ class GameObject():
                  start_pos: coords = (0, 0),
                  rotation: float = 0,
                  image: str = "assets/images/missing_texture_32x32") -> None:
-        self.tag: str | None = tag
-        self.position: coords = start_pos
-        self.rotation: float = rotation
-        self.image: str = image
+        self.tag: str | None = tag #When dealing with collisions, this should help.
+        self.position: coords = start_pos #The object's position.
+        self.rotation: float = rotation #The object's rotation. Rotation measured in Radians (NOT DEGREES)
+        self.image: str = image #A directory to the image associated with this object.
 
     def set_position(self, position: coords = (0, 0)) -> None:
+        """Sets the Object's position."""
         self.position = (float(position[0]), float(position[1]))
 
     def move_by_amount(self, position: coords) -> coords:
+        """Moves the Object's position by an amount. Returns new position."""
         self.position = (self.position[0] + position[0],
                          self.position[1] + position[1])
         return self.position
 
     def rotate_left_by(self, rotate_by: float) -> float:
+        """Rotates the object left. Returns new rotation."""
         self.rotation = self.rotate - rotate_by
         return self.rotation
 
     def rotate_right_by(self, rotate_by: float) -> float:
+        """Rotates the object right. Returns new rotation."""
         self.rotation = self.rotate + rotate_by
         return self.rotation
