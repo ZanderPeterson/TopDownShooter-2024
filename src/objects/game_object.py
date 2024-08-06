e3587aa733017969e566e34bdb488c8d826f5fa9
Zander Peterson <zander.j.peterson@gmail.com>
2024-08-07 10:33:22 +1200

    Flesh out some class features
    Added in some variables that tracks rotation and position. Also implemented a "tag" and "image". Also implemented trivial functions to change the position.

--- src/objects/game_object.py
+++ src/objects/game_object.py
@@ -1,8 +1,27 @@
+from typing import Tuple, TypeAlias
+
 import pygame
+
+coords: TypeAlias = Tuple[float, float]
 
 
 class GameObject():
     """A class intended to be inherited from to handle object related functions."""
 
-    def __init__(self):
-        pass
+    def __init__(self,
+                 tag: str | None = None,
+                 start_pos: coords = (0, 0),
+                 rotation: float = 0,
+                 image: str = "assets/images/missing_texture_32x32") -> None:
+        self.tag: str | None = tag
+        self.position: coords = start_pos
+        self.rotation: rotation
+        self.image: str = image
+
+    def set_position(self, position: coords = (0, 0)) -> None:
+        self.position = (float(position[0]), float(position[1]))
+
+    def move_by_amount(self, position: coords) -> coords:
+        self.position = (self.position[0] + position[0],
+                         self.position[1] + position[1])
+        return self.position
