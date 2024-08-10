428c44462ec24ab198b67a08f4f71b025947ec1e
Zander Peterson <zander.j.peterson@gmail.com>
2024-08-10 17:09:28 +1200

    Fixed Utils Again
    Fixed another issue with rotate_around_centre(), where rotations past half pi caused weird behaviour.

--- src/utils.py
+++ src/utils.py
@@ -11,7 +11,12 @@ def rotate_around_centre(width: float, height: float, rotation: float) -> coords
     Based upon the previous width and height, and the amount the image is being rotated this function
     finds the amount that the image needs to be moved inorder to maintain the centre's position.
     """
-    #'1' is just a hardcoded math.sin(math.pi/2).
+    if rotation >= 0:
+        rotation = rotation%(math.pi/2)
+    else:
+        rotation = -(rotation%(math.pi/2))
+
+    # '1' is just a hardcoded math.sin(math.pi/2).
     A: float = math.sin(math.pi/2 - rotation)/(1/height) + math.sin(rotation)/(1/width)
     B: float = math.sin(rotation)/(1/height) + math.sin(math.pi/2 - rotation)/(1/width)
     print(f"A: {A}, B: {B}")
