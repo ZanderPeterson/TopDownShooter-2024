20b8a49e7b39019a3a38bf573dbfdaf77551cdc5
Zander Peterson <zander.j.peterson@gmail.com>
2024-08-13 09:30:27 +1200

    Added __init__() method
    Added in the __init__() method for the PlayerObject subclass.

--- src/objects/player_object.py
+++ src/objects/player_object.py
@@ -2,5 +2,12 @@ import pygame
 
 from .game_object import GameObject
 
+
 class PlayerObject(GameObject):
-    pass
+    """A Subclass of GameObject which is intended to be used as the player."""
+
+    def __init__(self,
+                 start_pos: coords = (0, 0),
+                 rotation: float = 0,
+                 image: str | None = None) -> None:
+        super().__init__(tag="player", start_pos=start_pos, rotation=rotation, image=image)
