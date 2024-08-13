180aa965c38da33a3d87893c6490d118d7059637
Zander Peterson <zander.j.peterson@gmail.com>
2024-08-14 10:16:39 +1200

    PlayGame State now Tracks Keys
    Key presses and releases are now tracked in a way that makes referencing them in the update() method easy.
    
    A foreseeable issue is if the player holds down a key, goes into the PauseGame state and then goes back into the game, as this will cause a sticky key.

--- src/states/play_game.py
+++ src/states/play_game.py
@@ -1,5 +1,5 @@
 from math import pi
-from typing import Dict, override
+from typing import Dict, List, override
 
 import pygame
 
@@ -13,6 +13,12 @@ class PlayGameState(GameState):
     def __init__(self, game) -> None:
         super().__init__(game)
         self.entities: Dict[str, GameObject] = {}
+        self.track_keys: Dict[int, bool] = {
+            pygame.K_w: False,
+            pygame.K_a: False,
+            pygame.K_s: False,
+            pygame.K_d: False,
+        }
 
         self.entities["my_object"] = GameObject()
 
@@ -36,3 +42,11 @@ class PlayGameState(GameState):
         for entity in self.entities.values():
             #print(entity.get_image_position())
             window.blit(entity.render_image(), entity.get_image_position())
+
+    def handle_event(self, event) -> None | str:
+        # Record down keypresses.
+        if event.type == pygame.KEYDOWN and event.key in self.track_keys:
+            self.track_keys[event.key] = True
+        elif event.type == pygame.KEYUP and event.key in self.track_keys:
+            self.track_keys[event.key] = False
+        return None
