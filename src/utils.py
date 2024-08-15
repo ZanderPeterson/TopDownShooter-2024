0641076699b72e38525eb8251abd0c9d3f31916d
Zander Peterson <zander.j.peterson@gmail.com>
2024-08-15 12:10:47 +1200

    Added a Function to utils.py
    Added a function which takes two coordinates and finds what vector you need to take from the first point to get to the second point. This will be useful for the movement system.

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,13 @@ import math
 from typing import Tuple, TypeAlias
 
 coords: TypeAlias = Tuple[float, float]
+Vector: TypeAlias = Tuple[float, float] #Magnitude, Direction
+
+def find_vector_between(from_pos: coords, to_pos: coords) -> Vector:
+    if from_pos == to_pos:
+        return (0, 0)
+    x_difference = to_pos[0] - from_pos[0]
+    y_difference = to_pos[1] - from_pos[1]
+    magnitude = math.sqrt(x_difference**2 + y_difference**2)
+    direction = math.acos(x_difference / magnitude)
+    return (magnitude, direction)
