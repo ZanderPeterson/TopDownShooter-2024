e8b31b237a22e23895f9fb8fd3fd0340a28d6408
Zander Peterson <zander.j.peterson@gmail.com>
2024-08-21 11:58:45 +1200

    Added W&S Movement
    Added in the forwards and backwards movement & tested it.
    Tests worked well, although it is noted that there was some weird behaviour caused when the cursor was too close to the Player's Centre.

--- src/states/play_game.py
+++ src/states/play_game.py
@@ -38,13 +38,27 @@ class PlayGameState(GameState):
 
     @override
     def update(self) -> None:
-        FORWARD_SPEED = 2
+        #Define some variables... maybe not the best way to do it.
+        FORWARD_SPEED: float = 2
+        BACKWARD_SPEED: float = 1.5
+
+        #Gets position of the mouse, and finds the Vector from the centre of the player to it.
         mouse_pos = pygame.mouse.get_pos()
         vector_to_cursor: Vector = find_vector_between(self.entities["player"].centre, mouse_pos)
+
+        #Rotates the player to look at the mouse.
         self.entities["player"].set_rotation(vector_to_cursor[1])
-        move_by = move_by_vector((0, 0),
-                                 (min(vector_to_cursor[0], FORWARD_SPEED), vector_to_cursor[1]))
-        self.entities["player"].move_by_amount(move_by)
+
+        #W & S Movement Code
+        if not (self.track_keys[pygame.K_w] and self.track_keys[pygame.K_s]):
+            forward_back_speed: float = 0
+            if self.track_keys[pygame.K_w]:
+                forward_back_speed = FORWARD_SPEED
+            elif self.track_keys[pygame.K_s]:
+                forward_back_speed = -BACKWARD_SPEED
+            move_forward_by: Vector = move_by_vector((0, 0),
+                                                     (forward_back_speed, vector_to_cursor[1]))
+            self.entities["player"].move_by_amount(move_forward_by)
 
     @override
     def render(self, window) -> None:
