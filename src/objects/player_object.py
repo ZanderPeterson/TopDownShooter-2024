903db65cc39e0d5bd831338eea234833a3dc1cac
Zander Peterson <zander.j.peterson@gmail.com>
2024-08-30 12:36:51 +1200

    Created move_forward() in PlayerObject
    Created a move forwards function in the PlayerObject.

--- src/objects/player_object.py
+++ src/objects/player_object.py
@@ -3,8 +3,10 @@ from typing import Tuple, TypeAlias
 import pygame
 
 from .game_object import GameObject
+from src.utils import move_by_vector
 
 coords: TypeAlias = Tuple[float, float]
+Vector: TypeAlias = Tuple[float, float] #Magnitude, Direction
 
 
 class PlayerObject(GameObject):
@@ -15,3 +17,14 @@ class PlayerObject(GameObject):
                  rotation: float = 0,
                  image: str | None = None) -> None:
         super().__init__(tag="player", start_pos=start_pos, rotation=rotation, image=image)
+        self.forward_speed = 3
+        self.backward_speed = 3
+        self.sideways_speed = 100
+
+    def move_forward(self, vector_to_cursor: Vector, move_by: float | None = None) -> None:
+        """Moves the player forward (towards the cursor)."""
+        if not move_by:
+            move_by = self.forward_speed
+
+        change_pos_by: coords = move_by_vector((0, 0), (move_by, vector_to_cursor[1]))
+        self.move_by_amount(change_pos_by)
