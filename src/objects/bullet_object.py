896111c6be876634d290954248289c4c9f64e16d
Zander Peterson <zander.j.peterson@gmail.com>
2024-09-04 12:20:20 +1200

    Added Variable to BulletObject
    Updated BulletObject so that each instance of the class stored a vector. This vector would be moved by every frame.

--- src/objects/bullet_object.py
+++ src/objects/bullet_object.py
@@ -5,6 +5,7 @@ import pygame
 from .game_object import GameObject
 
 coords: TypeAlias = Tuple[float, float]
+Vector: TypeAlias = Tuple[float, float] #Magnitude, Direction
 
 
 class BulletObject(GameObject):
@@ -13,5 +14,9 @@ class BulletObject(GameObject):
     def __init__(self,
                  start_pos: coords = (0, 0),
                  rotation: float = 0,
+                 movement: Vector | None = None,
                  image: str | None = None) -> None:
         super().__init__(tag="bullet", start_pos=start_pos, rotation=rotation, image=image)
+        if not movement:
+            movement = (0, 0)
+        self.movement = movement
