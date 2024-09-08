7125084755a3a1b0e47d3e3226a709cb0a880463
Zander Peterson <zander.j.peterson@gmail.com>
2024-09-08 13:23:12 +1200

    Created Wall Constructor: right_of_wall()
    Created a constructor method for the wall object that takes in another wall and then returns a new wall that is to the right of the aforementioned wall.

--- src/objects/wall_object.py
+++ src/objects/wall_object.py
@@ -3,9 +3,11 @@ from typing import Tuple, TypeAlias
 import pygame
 
 from .game_object import GameObject
+from src.utils import find_object_corner, move_by_vector
 
 coords: TypeAlias = Tuple[float, float]
 Vector: TypeAlias = Tuple[float, float] #Magnitude, Direction
+size: TypeAlias = Tuple[float, float] #width, height
 
 
 class WallObject(GameObject):
@@ -16,3 +18,10 @@ class WallObject(GameObject):
                  rotation: float = 0,
                  image: str | None = None) -> None:
         super().__init__(tag="wall", start_pos=start_pos, rotation=rotation, image=image)
+
+    @classmethod
+    def right_of_wall(cls, ref_wall: WallObject, image: str | None) -> WallObject:
+        """This method constructs a new WallObject with a position to the right of another wall."""
+        object_corner = find_object_corner(ref_wall.position, ref_wall.centre, ref_wall.rotation)
+        new_wall_position = move_by_vector(object_corner, (ref_wall.img_size[0], ref_wall.rotation))
+        return cls(new_wall_position, self.rotation, image)
