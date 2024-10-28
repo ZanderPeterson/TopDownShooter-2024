c451851d08237f3f51abd66622faddd2aaf70d1f
Zander Peterson <zander.j.peterson@gmail.com>
2024-10-29 01:08:41 +1300

    Added Highscore

--- src/states/main_menu.py
+++ src/states/main_menu.py
@@ -11,6 +11,7 @@ class MainMenuState(GameState):
     def __init__(self, game) -> None:
         super().__init__(game)
         self.score: int = 0
+        self.high_score: int = 0
         self.h1: pygame.font.FontType = pygame.font.Font(None, 60)
         self.h2: pygame.font.FontType = pygame.font.Font(None, 40)
         self.h3: pygame.font.FontType = pygame.font.Font(None, 20)
@@ -18,6 +19,8 @@ class MainMenuState(GameState):
     @override
     def enter(self) -> None:
         print("Entering the Main Menu state.")
+        if self.score > self.high_score:
+            self.high_score = self.score
 
     @override
     def exit(self) -> None:
@@ -28,13 +31,15 @@ class MainMenuState(GameState):
         window.fill((0, 0, 0))
 
         title_text = self.h1.render(str(Settings.WINDOW_CAPTION), True, (255, 255, 255))
-        subtitle_text = self.h3.render("Created by Zander Peterson - Version 1.0", True, (255, 255, 255))
-        score_text = self.h2.render(f"Previous Score: {self.score}", True, (255, 255, 255))
+        subtitle_text = self.h3.render("Created by Zander Peterson - Version 1.0", True, (200, 200, 200))
+        score_text = self.h2.render(f"Previous Score: {self.score}", True, (200, 200, 200))
+        high_score_text = self.h2.render(f"Highest Score: {self.high_score}", True, (200, 200, 200))
         how_to_text = self.h1.render("Press [Space] To Start", True, (255, 255, 255))
 
         window.blit(title_text, (400 - title_text.get_size()[0] / 2, 100 - title_text.get_size()[1] / 2))
         window.blit(subtitle_text, (400 - subtitle_text.get_size()[0] / 2, 130 - subtitle_text.get_size()[1] / 2))
-        window.blit(score_text, (400 - score_text.get_size()[0] / 2, 300 - score_text.get_size()[1] / 2))
+        window.blit(score_text, (400 - score_text.get_size()[0] / 2, 275 - score_text.get_size()[1] / 2))
+        window.blit(high_score_text, (400 - high_score_text.get_size()[0] / 2, 325 - high_score_text.get_size()[1] / 2))
         window.blit(how_to_text, (400 - how_to_text.get_size()[0] / 2, 500 - how_to_text.get_size()[1] / 2))
 
     @override
